6e5e646e328a4bfa94f187bd087493365ce6225b
tiiffanyho <Tiffanyho0903@gmail.com>
2024-12-03 14:07:02 -0500

    bianry and insertion

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -5,6 +5,24 @@ public class InsertionSort {
      * Sort the array arr using insertion sort algorithm.
      */
     public static void sort(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
 
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
