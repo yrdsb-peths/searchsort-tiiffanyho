1ff294d2ee00d465d05fce680fe42e2a979883d6
tiiffanyho <Tiffanyho0903@gmail.com>
2024-12-09 13:02:56 -0500

    median

--- app/src/main/java/searchsort/Median.java
+++ app/src/main/java/searchsort/Median.java
@@ -10,21 +10,54 @@ public class Median {
      * Find the median of the array arr using QuickSort algorithm.
      */
     public double findQuick(int[] arr) {
-        return 0;
+        QuickSort quick = new QuickSort();
+        quick.sort(arr);
+        if(arr.length == 0) {
+            return 0.0;
+        }
+        int max = arr.length - 1;
+        int min = 0;
+
+        if(arr.length % 2 == 0) {
+            return (arr[(min + max) / 2] + arr[(min + max) / 2 + 1]) / 2.0;
+        }
+        return arr[(min + max) / 2];
     }
 
     /**
      * Find the median of the array arr using MergeSort algorithm.
      */
     public double findMerge(int[] arr) {
-        return 0;
+        MergeSort merge = new MergeSort();
+        merge.sort(arr);
+        if(arr.length == 0) {
+            return 0.0;
+        }
+        int max = arr.length - 1;
+        int min = 0;
+
+        if(arr.length % 2 == 0) {
+            return (arr[(min + max) / 2] + arr[(min + max) / 2 + 1]) / 2.0;
+        }
+
+        return arr[(min + max) / 2];
     }
 
     /**
      * Find the median of the array arr using InsertionSort algorithm.
      */
     public double findInsertion(int[] arr) {
-        return 0;
+        InsertionSort.sort(arr);
+        if(arr.length == 0) {
+            return 0.0;
+        }
+        int max = arr.length - 1;
+        int min = 0;
+
+        if(arr.length % 2 == 0) {
+            return (arr[(min + max) / 2] + arr[(min + max) / 2 + 1]) / 2.0;
+        }
+        return arr[(min + max)];
     }
 
 }
